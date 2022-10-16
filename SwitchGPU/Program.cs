0dafd4d2ef8335e14a01c98da70b265f800c0e55
Spylak <spylak@hotmail.com>
2022-10-16 22:39:00 +0300

    Set power mode to saver when on battery or balanced on AC

--- SwitchGPU/Program.cs
+++ SwitchGPU/Program.cs
@@ -7,7 +7,7 @@ eTriggerBattery.Subscription = @"<QueryList><Query Id='0' Path='System'><Select 
 eTriggerBattery.ValueQueries.Add("Name", "Value");
 tdOff.Triggers.Add(eTriggerBattery);
 
-tdOff.Actions.Add(new ExecAction(@"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe",arguments:string.Format("pnputil /disable-device \"{0}\"", "PCI\\VEN_1002'&'DEV_73EF'&'SUBSYS_1DCC1043'&'REV_C0\\6'&'E1D2937'&'0'&'00000009")));
+tdOff.Actions.Add(new ExecAction(@"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe",arguments:string.Format("pnputil /disable-device \"{0}\"", "PCI\\VEN_1002'&'DEV_73EF'&'SUBSYS_1DCC1043'&'REV_C0\\6'&'E1D2937'&'0'&'00000009")+";"+"powercfg /setactive 63c6ce89-e91a-4e48-8620-185e7d229f16"));
 tdOff.Settings.StopIfGoingOnBatteries = false;
 tdOff.Settings.DisallowStartIfOnBatteries = false;
 TaskService.Instance.RootFolder.RegisterTaskDefinition("OnBatteryGPU", tdOff);
@@ -18,7 +18,7 @@ EventTrigger eTriggerAC = new EventTrigger();
 eTriggerAC.Subscription = @"<QueryList><Query Id='0' Path='System'><Select Path='System'>*[EventData[Data[@Name='AcOnline']='true']]</Select></Query></QueryList>";
 eTriggerAC.ValueQueries.Add("Name", "Value");
 tdOn.Triggers.Add(eTriggerAC);
-tdOn.Actions.Add(new ExecAction(@"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe",arguments:string.Format("pnputil /enable-device \"{0}\"", "PCI\\VEN_1002'&'DEV_73EF'&'SUBSYS_1DCC1043'&'REV_C0\\6'&'E1D2937'&'0'&'00000009")));
+tdOn.Actions.Add(new ExecAction(@"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe",arguments:string.Format("pnputil /enable-device \"{0}\"", "PCI\\VEN_1002'&'DEV_73EF'&'SUBSYS_1DCC1043'&'REV_C0\\6'&'E1D2937'&'0'&'00000009")+";"+"powercfg /setactive 381b4222-f694-41f0-9685-ff5bb260df2e"));
 tdOn.Settings.StopIfGoingOnBatteries = false;
 tdOn.Settings.DisallowStartIfOnBatteries = false;
 TaskService.Instance.RootFolder.RegisterTaskDefinition("OnACGPU", tdOn);
